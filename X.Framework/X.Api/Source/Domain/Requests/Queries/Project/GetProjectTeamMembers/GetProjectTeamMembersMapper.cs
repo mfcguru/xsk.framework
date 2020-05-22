36ddcb2c6937c9dafd36ba499c4e1a1b6631acdd
mfcguru <mfguru@yahoo.com>
2020-05-22 16:07:36 +0800

    minor change

--- X.Framework/X.Api/Source/Domain/Requests/Queries/Project/GetProjectTeamMembers/GetProjectTeamMembersMapper.cs
+++ X.Framework/X.Api/Source/Domain/Requests/Queries/Project/GetProjectTeamMembers/GetProjectTeamMembersMapper.cs
@@ -9,7 +9,7 @@ namespace X.Api.Source.Domain.Requests.Queries
 			CreateMap<X.Api.Entities.TeamMember, GetProjectTeamMembersDto>()
 				.ForMember(dest => dest.MemberId, opt => opt.MapFrom(o => o.User.UserId))
 				.ForMember(dest => dest.MemberName, opt => opt.MapFrom(o => string.Format("{0} {1}", o.User.FirstName, o.User.LastName)))
-				.ForMember(dest => dest.MemberInitials, opt => opt.MapFrom(o => string.Format("{0} {1}", o.User.FirstName.ToUpper()[0], o.User.LastName.ToUpper()[0])))
+				.ForMember(dest => dest.MemberInitials, opt => opt.MapFrom(o => string.Format("{0}{1}", o.User.FirstName.ToUpper()[0], o.User.LastName.ToUpper()[0])))
 				;
 		}
 	}
