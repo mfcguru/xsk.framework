63c10a1f37cba3039a3b157d5ed3ef8553cf9743
mfcguru <mfguru@yahoo.com>
2020-05-22 14:42:57 +0800

    added comment field in edit task

--- X.Framework/X.Api/Source/Domain/Requests/Queries/Task/GetTaskById/GetTaskByIdMapper.cs
+++ X.Framework/X.Api/Source/Domain/Requests/Queries/Task/GetTaskById/GetTaskByIdMapper.cs
@@ -10,7 +10,7 @@ namespace X.Api.Source.Domain.Requests.Queries
             CreateMap<TaskItem, GetTaskByIdDto>()
                 .ForMember(dest => dest.TaskId, opt => opt.MapFrom(src => src.TaskItemId))
                 .ForMember(dest => dest.TaskName, opt => opt.MapFrom(src => src.TaskItemName))
-                .ForMember(dest => dest.CreateBy, opt => opt.MapFrom(src => src.CreatedByNavigation.Username))
+                .ForMember(dest => dest.CreateBy, opt => opt.MapFrom(src => string.Format("{0} {1}", src.CreatedByNavigation.Member.FirstName, src.CreatedByNavigation.Member.LastName)))
                 ;        
         }
     }
